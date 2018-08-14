3e38ed04660d8f43bb97d54155631bcaac5f7228
Kong To <newlight77@gmail.com>
2018-08-15 00:23:47 +0200

    activate data inject runner

--- oneprofile-admin/src/main/java/com/newlight77/admin/config/DataInjectionConfig.java
+++ oneprofile-admin/src/main/java/com/newlight77/admin/config/DataInjectionConfig.java
@@ -7,14 +7,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
-//@Configuration
-//@EnableNeo4jRepositories(basePackages = {"com.newlight77.admin.repository" })
-//@EntityScan(basePackages = "com.newlight77.admin.neo4j")
+@Configuration
+@EnableNeo4jRepositories(basePackages = {"com.newlight77.admin.repository" })
+@EntityScan(basePackages = "com.newlight77.admin.neo4j")
 public class DataInjectionConfig {
 
-//  @Bean
-//  CommandLineRunner userInjectionRunner() {
-//    return new UserInjectionRunner();
-//  }
+  @Bean
+  CommandLineRunner userInjectionRunner() {
+    return new UserInjectionRunner();
+  }
 
 }
