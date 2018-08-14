d39045521d600dc7f84d81e1c9403f3d46baee62
Kong To <newlight77@gmail.com>
2018-08-15 00:19:58 +0200

    change debug port for inject-data

--- demo/inject-admin-data.sh
+++ demo/inject-admin-data.sh
@@ -2,7 +2,7 @@
 
 
 CP_PARAM="-cp ./oneprofile-admin-data-injection/target/oneprofile-admin-data-injection-fat-exec.jar"
-DEBUG_PARAM="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5002"
+DEBUG_PARAM="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5003"
 LAUNCHER_CLASS="org.springframework.boot.loader.JarLauncher"
 
 JAVA_CMD="java $CP_PARAM $DEBUG_PARAM $LAUNCHER_CLASS"
